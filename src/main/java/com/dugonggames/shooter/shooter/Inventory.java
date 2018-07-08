79a2fdb260a33b234e6d187da1fdbbb670ec4cff
Nicolas Feltman <nfeltman@gmail.com>
2018-07-08 12:12:55 -0700

    Inventory skeleton

---
+++ src/main/java/com/dugonggames/shooter/shooter/Inventory.java
@@ -0,0 +1,24 @@
+package com.dugonggames.shooter.shooter;
+
+public class Inventory {
+
+    public enum Item {
+        SOME_POWERUP
+    }
+
+    public boolean hasAtLeastOne(Item i) {
+        throw new RuntimeException("not implemented");
+    }
+
+    public boolean getCount(Item i) {
+        throw new RuntimeException("not implemented");
+    }
+
+    public void increment(Item i) {
+        throw new RuntimeException("not implemented");
+    }
+
+    public void decrement(Item i) {
+        throw new RuntimeException("not implemented");
+    }
+}
